43dd1436521c241f18c9942674f83766cf41ce0f
otter-botter-417 <ren.finalfantasy@hotmail.co.jp>
2023-05-09 01:09:04 +0900

    chapter3 end

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,10 +9,11 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet" />
+        <script src="{{ mix('js/app.js') }}" defer></script>
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
